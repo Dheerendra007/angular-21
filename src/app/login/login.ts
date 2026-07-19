6d066e98f740be33f570e61e8ce280607f9f0be8
Dheerendra <dheerendra@Dheerendras-MacBook-Pro.local>
2026-07-19 20:59:03 +0530

    Add Event Binding

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -6,4 +6,20 @@ import { Component } from '@angular/core';
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  count = 0;
+  callLogin(action:String) {
+    if(action === 'minus') {
+      if(this.count  ==0) {
+      this.alertCount();
+      }
+     this.count>0 && this.count--;
+    }else{
+this.count++;
+    }
+    
+  }
+  alertCount() {
+    alert(`Current count is not leess than 0: ${this.count}`);
+  }
+}
