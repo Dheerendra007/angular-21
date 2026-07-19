078f7a81ab0693d36f4fdd1020820a3d92532313
Dheerendra <dheerendra@Dheerendras-MacBook-Pro.local>
2026-07-19 08:20:10 +0530

    Added login component

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,9 +5,18 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
+  /*template: `
+  <h1>{{name}}</h1>
+  <h1>{{title()}}</h1>
+  <h2>Welcome to {{name}}!</h2>
+  <h3>Sum of 10 and 20 is {{getAddition(10, 20)}}</h3>
+  `,*/
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('angular-21');
   name = 'Angular 21';
+  getAddition(a:number, b:number): number {
+    return a + b;
+  }
 }
