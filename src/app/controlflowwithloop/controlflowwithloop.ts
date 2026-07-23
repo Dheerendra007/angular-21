5e5b175eae56820cfabdb6f6b863406e1a5a3307
Dheerendra Kapariya <dkapariya@gmail.com>
2026-07-23 16:47:51 +0530

    loop with update colors and edit row

--- src/app/controlflowwithloop/controlflowwithloop.ts
+++ src/app/controlflowwithloop/controlflowwithloop.ts
@@ -8,4 +8,10 @@ import { Component, signal } from '@angular/core';
 })
 export class Controlflowwithloop {
   userList = signal(["Dheerendra","Thomas","Simon","Stark"])
+  userDetails = signal([
+    {id:1, name:"Dheerendra",emil:"Dheerendra@test.com"},
+    {id:1, name:"Thomas",emil:"Thomas@test.com"},
+    {id:1, name:"Simon",emil:"Simon@test.com"},
+    {id:1, name:"Stark",emil:"Stark@test.com"}
+  ])
 }
