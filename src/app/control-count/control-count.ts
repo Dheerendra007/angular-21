3218f00c4d32d1116e929fd4073a4926bf10a07a
Dheerendra Kapariya <dkapariya@gmail.com>
2026-08-01 05:30:30 +0530

    update count global level

---
+++ src/app/control-count/control-count.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-control-count',
+  imports: [],
+  templateUrl: './control-count.html',
+  styleUrl: './control-count.css',
+})
+export class ControlCount {}
---
+++ src/app/display-count/display-count.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-display-count',
+  imports: [],
+  templateUrl: './display-count.html',
+  styleUrl: './display-count.css',
+})
+export class DisplayCount {}
