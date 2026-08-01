88d0236eaa0052b7b47915bbbd2ab09ff3ec8097
Dheerendra Kapariya <dkapariya@gmail.com>
2026-08-02 00:02:59 +0530

    query param send value one page to other

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { Home } from './pages/home/home';
 import { Login } from './login/login';
 import { Profile } from './pages/profile/profile';
 import { About } from './pages/about/about';
+import { PageNotFound } from './pages/page-not-found/page-not-found';
 export const routes: Routes = [
     {path:"",component:Home},
     {path:"login",component:Login},
     {path:"profile",component:Profile},
     {path:"about",component:About},
+    {path:"**",component:PageNotFound},
 ];
--- src/app/pages/profile/profile.ts
+++ src/app/pages/profile/profile.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -6,4 +7,15 @@ import { Component } from '@angular/core';
   templateUrl: './profile.html',
   styleUrl: './profile.css',
 })
-export class Profile {}
+export class Profile {
+
+
+  constructor(public route:ActivatedRoute ) { }
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      console.log(params); // { name: "John Doe", age: "30" }
+      console.log(params['name']); // John Doe
+      console.log(params['age']); // 30
+    });
+  } 
+}
