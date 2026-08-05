fd64e050c54adb6378142e836bc3864e933edcb0
Dheerendra Kapariya <dkapariya@gmail.com>
2026-08-05 22:38:47 +0530

    add user data

--- src/app/services/user-service.ts
+++ src/app/services/user-service.ts
@@ -7,7 +7,11 @@ export class UserService {
             { id: 1, name: 'John Doe' , email: 'john.doe@example.com' ,city: 'New York', country: 'USA' },
             { id: 2, name: 'Jane Smith' , email: 'jane.smith@example.com' ,city: 'Los Angeles', country: 'USA' },
             { id: 3, name: 'Bob Johnson' , email: 'bob.johnson@example.com' ,city: 'Chicago', country: 'USA' },
-            { id: 4, name: 'Alice Williams' , email: 'alice.williams@example.com' ,city: 'Houston', country: 'USA' }
+            { id: 4, name: 'Alice Williams' , email: 'alice.williams@example.com' ,city: 'Houston', country: 'USA' },
+            { id: 5, name: 'Charlie Brown' , email: 'charlie.brown@example.com' ,city: 'Phoenix', country: 'USA' },
+            { id: 6, name: 'Lavya' , email: 'lavya@example.com' ,city: 'San Francisco', country: 'USA' },
+            { id: 7, name: 'Dheerendra' , email: 'dheerendra@example.com' ,city: 'Austin', country: 'USA' },
+            { id: 8, name: 'Leena' , email: 'leena@example.com' ,city: 'Seattle', country: 'USA' },
         ];
     }
 }
