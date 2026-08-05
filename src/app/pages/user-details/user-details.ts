255b58486c88054d590b0c504b53909d743104bf
Dheerendra Kapariya <dkapariya@gmail.com>
2026-08-05 09:00:23 +0530

    open user profile and share data

--- src/app/pages/user-details/user-details.ts
+++ src/app/pages/user-details/user-details.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { UserService } from '../../services/user-service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-details',
   imports: [],
   templateUrl: './user-details.html',
-  styleUrl: './user-details.css',
+  styleUrls: ['./user-details.css'],
 })
-export class UserDetails {}
+export class UserDetails {
+  userData: any = signal("");
+
+  constructor(public userService: UserService, public route: ActivatedRoute) {}
+
+  ngOnInit() {
+    console.log(this.userService.userList());
+    this.route.params.subscribe(params => {
+      const userId = params['id'];
+      const data = this.userService.userList();
+      const userDetails = data.find((user: any) => user.id === parseInt(userId, 10));
+      this.userData.set(userDetails);
+    });
+  }
+}
--- src/app/pages/users/users.ts
+++ src/app/pages/users/users.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { UserService } from '../../services/user-service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-users',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './users.html',
   styleUrl: './users.css',
 })
