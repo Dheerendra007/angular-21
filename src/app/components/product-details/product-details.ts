e995f7071b73ad5f24468555d7e7549e96012b55
Dheerendra Kapariya <dkapariya@gmail.com>
2026-08-15 23:52:45 +0530

    button

--- src/app/components/product-details/product-details.ts
+++ src/app/components/product-details/product-details.ts
@@ -2,10 +2,13 @@ import { Component, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Products } from '../../services/products';
 import { MatSlideToggle } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-product-details',
-  imports: [MatSlideToggle],
+  imports: [MatButtonModule, MatDividerModule, MatIconModule, MatSlideToggle],
   templateUrl: './product-details.html',
   styleUrl: './product-details.css',
 })
